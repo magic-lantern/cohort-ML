f02fd2f2c1766d2e54615ababa19f5b4c20bed37
vector-client-token <>
2020-12-11 20:09:49 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -3,10 +3,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import sklearn.cluster as cluster
-from sklearn.metrics import adjusted_rand_score, adjusted_mutual_info_score, accuracy_score, confusion_matrix, plot_roc_curve, roc_auc_score, roc_curve, auc
+from sklearn.metrics import confusion_matrix, plot_roc_curve, roc_auc_score, roc_curve, auc, accuracy_score, balanced_accuracy_score, precision_score, recall_score
 from sklearn.model_selection import train_test_split, GridSearchCV, KFold
 from sklearn.feature_selection import RFE, RFECV
 from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import make_pipeline
 
 import timeit
 
@@ -15,10 +21,89 @@ import xgboost as xgb
 # set standard random state for repeatability
 my_random_state = 42
 
+def fit_and_report(estimator=None, label='', datadict={}, features=[]):
+    x_test = datadict['x_test']
+    y_test = datadict['y_test']
+    x_train = datadict['x_train']
+    y_train = datadict['y_train']
+    estimator.fit(x_train, y_train)
+
+    # summarize the selection of the attributes
+    importances = estimator.feature_importances_
+    indices = np.argsort(importances)[::-1]
+
+    for f in range(x_train.shape[1]):
+        print("%2d) %-*s %f" % (f + 1, 30, 
+                                features[indices[f]], 
+                                importances[indices[f]]))
+
+    y_pred = estimator.predict(x_test)
+    confmat = confusion_matrix(y_true=y_test, y_pred=y_pred)
+    print(label)
+    print(confmat)
+    print('Balanced Accuracy:', balanced_accuracy_score(y_test, y_pred))
+    print('Precision:', precision_score(y_test, y_pred))
+    print('Recall:', recall_score(y_test, y_pred))
+    y_pred = estimator.predict_proba(x_test)[:, 1]
+    print('ROC_AUC_SCORE: ', roc_auc_score(y_true=y_test, y_score=y_pred))
+
 @transform_pandas(
-    Output(rid="ri.vector.main.execute.8006baaa-9271-4b84-8282-bb82a1ce4b40"),
-    data_scaled_and_outcomes=Input(rid="ri.foundry.main.dataset.b474df3d-909d-4a81-9e38-515e22b9cff3")
+    Output(rid="ri.foundry.main.dataset.e4922e37-cdb5-4f8b-ae2a-bb41c11dcada"),
+    data_encoded_and_outcomes=Input(rid="ri.foundry.main.dataset.32069249-a675-4faf-9d3c-a68ff0670c07"),
+    data_scaled_and_outcomes=Input(rid="ri.foundry.main.dataset.b474df3d-909d-4a81-9e38-515e22b9cff3"),
+    inpatient_encoded_w_imputation=Input(rid="ri.foundry.main.dataset.d3578a81-014a-49a6-9887-53d296155bdd"),
+    inpatient_scaled_w_imputation=Input(rid="ri.foundry.main.dataset.f410db35-59e0-4b82-8fa8-d6dc6a61c9f2"),
+    outcomes=Input(rid="ri.foundry.main.dataset.3d9b1654-3923-484f-8db5-6b38b56e290c")
 )
-def unnamed(data_scaled_and_outcomes):
-    
+def generate_models_and_summary_info(data_scaled_and_outcomes, inpatient_scaled_w_imputation, data_encoded_and_outcomes, outcomes, inpatient_encoded_w_imputation):
+    # this set is for tree based methods that do not need/require scaling of the input data
+    # categoricals have been one-hot encoded, imputation done, but no scaling
+    data_and_outcomes = data_encoded_and_outcomes.toPandas()
+    my_data_enc = data_and_outcomes.select(inpatient_encoded_w_imputation.columns).toPandas()
+    my_data_enc = my_data_enc.drop(columns='visit_occurrence_id')
+    my_outcomes = data_and_outcomes.select(outcomes.columns).toPandas()
+    y = my_outcomes.bad_outcome
+    x_train_enc, x_test_enc, y_train, y_test = train_test_split(my_data, y, test_size=0.3, random_state=my_random_state, stratify=y)
+    data_enc = ={'x_train': x_train_enc, 'x_test': x_test_enc, 'y_train': y_train, 'y_test': y_test}
+
+    # this version has alredy had StandardScaler applied to the data
+    # after one-hot encoding, imputation
+    data_and_outcomes_std = data_scaled_and_outcomes.toPandas()
+    my_data_std = data_and_outcomes.select(inpatient_scaled_w_imputation.columns).toPandas()
+    my_data_std = my_data_std.drop(columns='visit_occurrence_id')
+    # y is just a binary outcome, so overwriting from previous train_test_split is ok
+    x_train_std, x_test_std, y_train, y_test = train_test_split(my_data, y, test_size=0.3, random_state=my_random_state, stratify=y)
+    data_std = ={'x_train': x_train_std, 'x_test': x_test_std, 'y_train': y_train, 'y_test': y_test}
+
+    #########################
+    # Random Forest
+    # best features from grid search: {'criterion': 'gini', 'max_features': 'sqrt', 'min_samples_split': 5, 'n_estimators': 750}
+    #########################
+    rf = RandomForestClassifier(n_estimators=750,
+                                min_samples_split=5,
+                                random_state=my_random_state,
+                                max_features='sqrt',
+                                criterion='gini')
+    fit_and_report(estimator=rf, label='RandomForest', datadict=data_enc, features=df.columns)
+
+    # fig, (ax1, ax2) = plt.subplots(2, figsize=(12,10))
+    # fig.tight_layout(h_pad=4)
+
+    # rf_disp = plot_roc_curve(rf, x_test, y_test, ax=ax1)
+
+    # ax2.set_title('Feature Importance')
+    # ax2.bar(range(x_train.shape[1]), 
+    #         importances[indices],
+    #         align='center')
+
+    # #plt.sca(ax2)
+    # #ax2.set_xticklabels(my_data.columns[indices])
+    # #plt.xticks(range(x_train.shape[1]), my_data.columns[indices], rotation=90)
+    # ax2.set_xticks(range(x_train.shape[1]))
+    # ax2.set_xticklabels(list(my_data.columns[indices]), rotation=-45, ha='left', fontsize=10)
+    # ax2.set_xlim([-1, x_train.shape[1]])
+
+    # plt.subplots_adjust(bottom=0.2)
+
+    # plt.show()
 
