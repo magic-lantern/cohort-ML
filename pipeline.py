8341a141b7e9cc68f95440528727282b23541484
vector-client-token <>
2020-12-14 23:28:32 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -111,7 +111,7 @@ def generate_models_and_summary_info(data_scaled_and_outcomes, inpatient_scaled_
     # this version has alredy had StandardScaler applied to the data
     # after one-hot encoding, imputation
     data_and_outcomes_std = data_scaled_and_outcomes
-    my_data_std = data_and_outcomes.select(inpatient_scaled_w_imputation.columns).toPandas()
+    my_data_std = data_and_outcomes_std.select(inpatient_scaled_w_imputation.columns).toPandas()
     # outcome
     y = my_outcomes.bad_outcome
     # split dataset
