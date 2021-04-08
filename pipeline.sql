46a2080633ad69dfeabc1d18f016502b2f7ca232
vector-client-token <>
2021-04-08 15:55:48 +0000

    Update workbook

--- pipeline.sql
+++ pipeline.sql
@@ -1,9 +1,10 @@
 
 
 @transform_pandas(
-    Output(rid="ri.vector.main.execute.2a07a055-a516-4591-befb-fe274141f319"),
+    Output(rid="ri.foundry.main.dataset.bca704bf-dd62-41d9-81b8-bf01814f578f"),
     outcomes=Input(rid="ri.foundry.main.dataset.349f1404-e60e-4a76-9a32-13fe06198cc1")
 )
-SELECT *
+SELECT count(1) as cnt, bad_outcome
 FROM outcomes
+group by bad_outcome
 
